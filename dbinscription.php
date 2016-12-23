0f79d7b5b667eef9aeb4b3046529d5af0041d183
manfrenc2 <manfrenc2@gmail.com>
2016-12-23 11:38:56 +0100

    dbinscription

--- dbinscription.php
+++ dbinscription.php
@@ -1,5 +1,5 @@
 <?php
- 	include_once 'dbconfig.php'; // appel du fichier de connexion à la db
+	include_once 'dbconfig.php'; // appel du fichier de connexion à la db
 
 if (isset($_POST["submit"])) { //On vérifie que l'utilisateur a bien rempli le champs "mail".
     $pseudoi = $_POST["mail"]; // On stock le mail entré par l'utilisateur dans la variable pseudoi.
@@ -15,8 +15,6 @@ if (isset($_POST["submit"])) { //On vérifie que l'utilisateur a bien rempli le 
   	$aide = $_POST["aide"]; //je récupère la valeur de l'input aide formation
   	$orgaide = $_POST["orgaide"]; //je récupère la valeur de l'input organisme d'aide
   	$montaide = $_POST["montaide"]; //je récupère la valeur de l'input montant de l'aide
-  	$carteid = $_POST["carteid"]; //je récupère la valeur de l'input carte identité
-  	$photoid = $_POST["photoid"]; //je récupère la valeur de l'input photo
   	$paiement = $_POST["choosepay"]; //je récupère la valeur de l'input paiement
   	$sex = $_POST["sexe"]; //je récupère la valeur de l'input sexe
   	$name = mysql_real_escape_string($_POST["name"]); //je récupère la valeur de l'input nom
@@ -31,9 +29,31 @@ if (isset($_POST["submit"])) { //On vérifie que l'utilisateur a bien rempli le 
   	$alleregi = $_POST["alleregi"]; //je récupère la valeur de l'input allergie
 
 
+  	foreach ($_FILES as $key => $value) {
+	    $nomOrigine = $_FILES[$key]['name'];
+	    $elementsChemin = pathinfo($nomOrigine);
+	    $extensionFichier = $elementsChemin['extension'];
+	    $extensionsAutorisees = array("jpeg", "jpg", "gif");
+	    if (!(in_array($extensionFichier, $extensionsAutorisees))) {
+	        echo "Le fichier n'a pas l'extension attendue";
+	    } else {    
+	        // Copie dans le repertoire du script avec un nom
+	        // incluant l'heure a la seconde pres 
+	        $repertoireDestination = dirname(__FILE__)."/";
+	        $nomDestination = "fichier_du_".date("YmdHis")."-".$key.".".$extensionFichier;
 
-
-
+	        if (move_uploaded_file($_FILES[$key]["tmp_name"], 
+	                                         $repertoireDestination.$nomDestination)) {
+	            echo "Le fichier temporaire ".$_FILES[$key]["tmp_name"].
+	                    " a été déplacé vers ".$repertoireDestination.$nomDestination;
+	        } else {
+	            echo "Le fichier n'a pas été uploadé (trop gros ?) ou ".
+	                    "Le déplacement du fichier temporaire a échoué".
+	                    " vérifiez l'existence du répertoire ".$repertoireDestination;
+	        }
+	    }
+  	}
+/*
 
     $result2 = mysqli_query($conn, $verif); //Envoi de la requête à la base de donnée.
     if(mysqli_num_rows($result2) > 0) { // Si la base de donnée nous retourne au moins un élément (ce qui signifie que le pseudo existe déjà), alors exécuter la suite :
@@ -41,14 +61,13 @@ if (isset($_POST["submit"])) { //On vérifie que l'utilisateur a bien rempli le 
         echo "ce pseudo existe déjà";
         die;
     }else { // Sinon
-    	$sql = "INSERT INTO stagiaire(`nom`, `prenom`, `date de naissance`, `adresse`, `code_postal`, `Ville`, `num_inscription_JS`, `num_securite_social`, `tel`, `Email`, `sexe`, `statut`, `mdp`, `membreAssoFSCF`, `numeroLicence`, `membreStructurePartenaire`, `priseEnChargeFinanciere`, `organisme`, `attestationInscription_idattestationInscription`, `personne_urgence`, `tel_urgence`, `medical`) VALUES ('$name','$firstname','$date','$adress','$codepo','$city','$numjs','$secu','$tel','$pseudoi','$sex','','','','$licence','$aide','$montaide','$orgaide',1,'$contacturg','$telurg','$alleregi')";
+    	$sql = "INSERT INTO stagiaire(`formation`,`nom`, `prenom`, `date de naissance`, `adresse`, `code_postal`, `Ville`, `num_inscription_JS`, `num_securite_social`, `tel`, `Email`, `sexe`, `statut`, `mdp`, `numeroLicence`, `priseEnChargeFinanciere`, `montantaide`, `organisme`, `personne_urgence`, `tel_urgence`, `medical`, `attestation`) VALUES ('$formation','$name','$firstname','$date','$adress','$codepo','$city','$numjs','$secu','$tel','$pseudoi','$sex','','','$licence','$aide','$montaide','$orgaide','$contacturg','$telurg','$alleregi',1)";
       //$sql2 = "INSERT INTO `client`(`email`, `pass`) VALUES ('$pseudoi','".crypt_password($mdpi)."')"; //Requête qui permet d'insérer un nouvel utilisateur désigné par son mail et mot de passe (que l'on crypte), on entre pas l'id puisque cette dernière s'autoincrémente déjà.
         mysqli_query($conn, $sql); // Envoie de la requête à la base de donnée.
         echo "ok<br>".$sql;
         die;
-    }
+    }*/
 }
-
 
 
 
