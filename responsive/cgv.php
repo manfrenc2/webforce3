d7e2a44bd88d0eaf4d3723b77549fc6dbd36722f
Celine <celine-roy@orange.fr>
2017-01-12 15:27:50 +0100

    CGV Responsive 1.0.1

---
+++ responsive/cgv.php
@@ -0,0 +1,68 @@
+<!-- barre de header principale avec logo, inscription et connexion -->
+<?php require ('header.php'); ?>
+
+	<!-- debut section avec le texte -->
+	<section class="col-md-6 col-xs-6 cgvplace">
+		<h1 class="titreprofil">CONDITIONS GÉNÉRALES DE VENTE</h1>					
+
+			<p>
+				<br/>
+				<h4 class="yellow2"><b>	Inscription et suivi administratif de votre dossier :</b></h4>
+				<br/>
+				A réception du formulaire d'inscription, le stagiaire reçoit un accusé de réception mentionnant l'état du dossier (validé, en attente ou refusé).Une fois le dossier validé, une attestation d'inscription, le projet pédagogique, une facture et la convocation (indiquant le lieu, les horaires, le matériel à apporter...).
+				<br/>
+				<br/>
+				<h4 class="blue2"><b>Modalités de paiement :</b></h4>
+				<br/>
+				<br/>
+				les tarifs présentés sont fixes et valables pour la période calendaire. Un devis peut être édités sur demande du stagiaire. Le paiement du sage est exigé à l'inscription. Le montant du stage comprend  150 €uros d'arrhes. L'organisateur accepte plusieurs paiements, à la condition que l'ensemble du solde soit en possession de l'organisateur à l'inscription. Les paiements peuvent se faire en ligne par carte bancaire, paypal, par chèque libellé au nom de la FSCF, par virement ou par espèces. Pour tout autre dispositif de paiement merci de nous contacter.
+				<br/>
+				<br/>
+				<h4 class="red2"><b>Désistement du stagiaire :</b></h4>
+				<br/>
+				<br/>
+				Conformément à l'article L114-1 du Code du commerce, il est important de souligner que l'inscription est assortie d'arrhes et qu'en conséquence, en cas de rétractation du stagiaire ou de son représentant légal, l'organisateur se garde le droit de lui demander en sus de la perte de celles-ci, le paiement de dommages et intérêts supplémentaires.
+				Avant le début du stage :
+				Lors d'un désistement 15 jours ouvrés avant la session, la totalité des arrhes sont rendus au stagiaire.
+				Lors d'un désistement intervenant entre 15 jours et le premier jour de la session, le montant des arrhes est totalement dû et l'organisateur se réserve le droit de facturer le montant correspondant aux frais d'hébergement et de restauration.
+				<br/>
+				<br/>
+				<h4 class="magenta2"><b>En cours de stage :</b></h4>
+				<br/>
+				<br/>
+				Lors d'un départ pendant un stage, la totalité de la somme reste due à l'organisateur. Si ce départ est lié à un motif spécifique (ex: décès d'un proche ou état médical ne permettant pas de poursuivre la session), les arrhes restent acquises et un forfait équivalent à 70€ par jour commencé est facturé.
+				<br/>
+				<br/>
+				<h4 class="yellow2"><b>Annulation d'une session :</b></h4>
+				<br/>
+				<br/>
+				L'organisateur se réserve le droit d'annuler une session ou en annuler les modalités d'organisation (dates, lieu, type de session, thèmes...). Dans ce cas, les participants sont informés par courrier ou par téléphone dans les plus brefs délais et l'organisateur propose aux participants une solution personnalisée.     
+				Dans le cas où aucune solution n'est trouvée entre les deux parties, les arrhes versées par le stagiaire sont remboursées intégralement par l'organisateur.
+				<br/>
+				<br/>
+				<h4 class="blue2"><b>Avances de frais frais médicaux :</b></h4>
+				<br/>
+				<br/>
+				Lorsque cela est nécessaire, l'organisateur avance les frais médicaux nécessités par l'état de santé  d'un stagiaire mineurs. Le remboursement de ces frais se fait alors le dernier jour, auprès du directeur du stage.
+				<br/>
+				<br/>
+				<h4 class="red2"><b>Horaires et rythmes des sessions :</b></h4>
+				<br/>
+				<br/>
+				Se former au BAFA, BAFD et à l'animation volontaire -même occasionnelle- nécessite une grande disponibilité. De façon a faire vivre aux stagiaires une situation la plus réelle possible, l'organisateur propose un rythme identique à celui d'un Accueil Collectif de Mineurs. De ce fait les journées débutent à 9h00  et se terminent à 19h30 (sous réserve de modification) pour les sessions en demi-pension, et après la veillée pour les sessions en internat (environ 23h). Lors des sessions en demi-pension des temps d'animation peuvent être organisés  en soirée, à la demande des équipes de formation. La participation à tous les temps de travail est obligatoire et un retard ou une absence peut justifier un renvoi de la session et/ou une appréciation non-satisfaisante.
+				<br/>
+				<br/>
+				<h4 class="magenta2"><b>Respect du règlement intérieur de la session :</b></h4>
+				<br/> 
+				<br/>
+				Tout manquement grave à la discipline, mettant notamment en danger la sécurité physique, affective ou morale d'un ou plusieurs stagiaires, sera signalé à la famille ou au responsable légal. Le renvoi du stagiaire peut alors être prononcé. Dans ce cas, la totalité du paiement reste acquise à l'organisateur et les frais occasionnés par le retour du stagiaire à son domicile sont à la charge de la famille ou du tuteur légal. Par ailleurs, conformément aux dispositions légales en vigueur, tout acte répréhensible pénalement fait l'objet d'un signalement aux autorités compétentes (forces de police, douanes, Direction Régionale de la Jeunesse, des Sports et de la cohésion Sociale).
+				Les stagiaires restent responsables de leurs affaires personnelles pendant les formations. En conséquence, l'organisateur déclinera toute responsabilité en cas de perte ou de vol.          
+				<br/>
+				<hr/>
+				<br/>
+				Les informations recueillies sont nécessaires pour votre  inscription et/ou adhésion. Elle font l'objet d'un traitement informatique et sont destinées au secrétariat de la fédération. En application des articles 39 et suivants de la loi du 6 janvier 1978 modifiée, vous bénéficiez d'un droit d'accès et de rectification aux informations qui vous concernent. Si vous souhaitez exercer ce droit et obtenir communication des informations vous concernant, veuillez vous adresser à juridique@fscf.asso.fr                                                                                        
+			</p>
+		</section>
+		<!-- fin de la section cgu -->
+</div>	
+<?php require('footer.php')  ?>
