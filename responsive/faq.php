9f2badc81167863d42d59f2e3e01a6351d05bada
Celine <celine-roy@orange.fr>
2017-01-13 08:49:37 +0100

    FAQ responsive 1.0.1

---
+++ responsive/faq.php
@@ -0,0 +1,199 @@
+<?php require ('header.php'); ?>
+					
+			<!-- Début du contenu principal de la page foire aux questions  -->					
+	<section>
+		<h1 class="title"> FAQ </h1>	
+		<div class="container">
+				<div class="alert alert-warning alert-dismissible fade in" role="alert"><!-- Message alertant les utilisateurs à se référer à la page "contact" si ils ont des questions supplémentaires-->
+					<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+				 Si vous ne trouvez aucune question à votre réponse, merci de nous contacter à l'endroit prévu en haut dans la barre de navigation.
+				</div><br>
+				<div class="panel-group" id="accordion">
+					<div class="faqHeader">Questions générales </div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title"><!-- Question BAFA  -->	
+								<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Comment se déroule le BAFA ?</a>
+							</h4>
+						</div>
+						<div id="collapseOne" class="panel-collapse collapse">
+							<div class="panel-body">
+								<p>Le <strong>BAFA</strong> se déroule en 3 parties :  la formation générale (8 jours), le stage pratique (ce stage doit s'effectuer dans un accueil de loisirs ou un séjour de vacances pendant 14 jours), la session d'approfondissement (6 jours) ou de qualification (8 jours).
+								Ces 3 stages doivent impérativement s'effectuer dans l'ordre et en France.</p>
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title"> <!-- Question BAFD -->	
+								<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">Comment se déroule le BAFD ?</a>
+							</h4>
+						</div>
+						<div id="collapseTwo" class="panel-collapse collapse">
+							<div class="panel-body">
+								Le <strong>BAFD</strong>  se déroule en 4 parties: la formation générale (9 jours), le premier stage pratique (ce stage de 14 jours peut être effectué en direction ou en adjoint de direction dans un Accueils Collectifs de Mineurs en France),un stage de perfectionnement (6 jours) et le 2 ème stage pratique (ce stage de 14 jours doit être effectué en direction dans un ACM en France). A l'issu de ces 4 étapes, le candidat dépose à la Direction Régionale de la Jeunesse des Sports et de la Cohésion Sociale un bilan de formation. 
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title"><!-- Question A QUOI CA SERT   -->	
+								<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">A quoi ça sert ?  </a>
+							</h4>
+						</div>
+						<div id="collapseThree" class="panel-collapse collapse">
+							<div class="panel-body">
+								 Le <strong>BAFA</strong> permet de travailler à titre non professionnel dans des accueils collectifs de mineurs (centres de loisirs ou colonies de vacances).
+								 Le <strong>BAFD</strong> est le seul diplôme non professionnel qui permet d'être cadre dans des accueils collectifs de mineurs.
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title">  <!-- Question diplôme -->	
+								<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour">Comment obtenir son diplôme ? </a>
+							</h4>
+						</div>
+						<div id="collapseFour" class="panel-collapse collapse">
+							<div class="panel-body">
+								Pour obtenir son diplôme, toutes les étapes du <strong>BAFA </strong> ou  <strong>BAFD </strong> doivent être validés puis votre dossier sera présenté devant un jury.							
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title">  <!-- Question âge -->	
+								<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFive">A partir de quel âge peut-on accéder à la formation BAFA /BAFD ? </a>
+							</h4>
+						</div>
+						<div id="collapseFive" class="panel-collapse collapse">
+							<div class="panel-body">
+								<p>Il est impératif d'avoir <strong>17 ans</strong> révolu au premier jour du stage de formation Générale BAFA, mais il est possible de s'inscrire avant à la formation.
+								On peut accéder à la formation BAFD à partir de <strong>21 ans</strong> révolu, il faut également être en possession du BAFA.</p> 
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title"> <!-- Question n° jeunesse et sport -->
+								<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSix">Où chercher mon numéro de dossier d'inscription Jeunesse et Sports ? </a>
+							</h4>
+						</div>
+					
+						<div id="collapseSix" class="panel-collapse collapse">
+							<div class="panel-body">
+								<p>Le numéro de jeunesse et sport est indispensable pour s'inscrire au BAFA ou BAFD. Si vous souhaiter par la suite vous inscrire au BAFD, il est nécessaire de faire une autre demande de numéro de jeunesse et sport car ce dernier est différent de celui pour le BAFA. 
+								<br>Pour le récuperer, il suffit de <strong>cliquer sur le lien</strong> suivant et de suivre les indications : 
+															<a class="color" href="http://www.jeunes.gouv.fr/bafa-bafd" target="_blank">http://www.jeunes.gouv.fr/bafa-bafd</a>
+									</p>
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title"> <!-- Question argent  -->
+								<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSeven">Combien ça coûte ? </a>
+							</h4>
+						</div>
+						<div id="collapseSeven" class="panel-collapse collapse">
+							<div class="panel-body">
+								<p> Le tarif est fonction du stage et de sa formule en pension complète ou demie pension. Ces informations se trouvent dans <strong>le calendrier des stages</strong> présent sur la page d'accueil ou la page des différentes formations. </p>
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title"> <!-- Question aide  -->
+								<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseEight">Quelles sont les aides financières ?
+								</a>
+							</h4>
+						</div>
+						<div id="collapseEight" class="panel-collapse collapse">
+							<div class="panel-body">
+							<p>  Certains organismes peuvent vous aider à financer votre Formation BAFA : </p>
+							<ul TYPE="square">
+								<li><p> <strong>La CAF</strong> : une aide nationale Dès confirmation de votre inscription à la 3ème session de la formation, la Caisse Nationale d’Allocations Familiales peut accorder une aide financière (pour tous, sans condition de ressources) Pour en bénéficier, télécharger et imprimer le dossier de demande CAF <a class="color" href="http://www.caf.fr/sites/default/files/caf/121/Documents/demande_daide_bafa.pdf" target="_blank">http://www.caf.fr/sites/default/files/caf/121/Documents/demande_daide_bafa.pdf</a></p> Faites compléter ce document à chaque étape de votre formation </p> </li>
+								</br> 
+								<li><p> <strong>La Mutuelle Sociale Agricole</strong> (M.S.A.) : Elle est destinée aux jeunes allocataires MSA et n’est pas soumises à conditions de ressources. Pour bénéficier de cette aide, si vous correspondez aux critères d’éligibilité, prenez contact avec votre caisse de Mutuelle Social Agricole dont dépend votre lieu d’habitation. </p></br></li>
+								<li><p> <strong>Le Conseil Départemental de l’Yonne</strong> : Pour les jeunes de 17 à 21 ans révolus domiciliés dans le département de l’Yonne, il n’y a pas de conditions de ressources minimum des familles. Pour bénéficier de cette aide, télécharger et imprimer le dossier d’inscription et la Convention Tripartite à retourner avec votre dossier d’inscription : <a class="color" href="http://www.lyonne.com/Sports-et-Jeunesse/Jeunesse/Aide-au-BAFA-et-au-BAFD " target="_blank">http://www.lyonne.com/Sports-et-Jeunesse/Jeunesse/Aide-au-BAFA-et-au-BAFD </a> </p></br></li>
+								<li><p> <strong>Le Pôle Emploi</strong> peut prendre en charge tout ou une partie de votre formation. Prenez contact avec votre conseiller pour étudier votre dossier et vos droits à la formation. </p></br></li>
+								<li><p> <strong>Les Comités d’entreprise</strong> </p></br></li>
+								<li><p> <strong>Les Communautés de Communes</strong> </p></br></li>
+								<li>	<p><strong> Votre employeur</strong></p></br></li>
+								<li><p> Pour plus de facilité, il est également possible de <strong>payer en trois fois</strong>, contactez nous pour plus d'informations. </p></li>
+								</ul>													
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title"> <!-- Question paiement après site  -->
+								<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseNine">Que se passe t-il lorsque j'ai payé sur le site ?  </a>
+							</h4>
+						</div>
+						<div id="collapseNine" class="panel-collapse collapse">
+							<div class="panel-body">
+								Après l'inscription, <strong>selon votre moyen de paiement</strong>, vous serez redirigéré sur une page de paiement puis un email de confirmation ou vous recevrez immédiatement un email sans passer par le paiement. Une vérification des papiers fournis est nécessaire pour confirmer l'inscription. 
+								Si vous bénéficier d'une aide, celle-ci sera réduite du montant à payer mais vous devrez nous envoyer un chèque d'ARRHES du montant de la bourse qui ne sera encaisser qu'au cas où l'aide est refusé <strong>(toutes les informations complètes se trouveront dans le mail)</strong>.
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title"> <!-- Question internat ou demi pension   -->
+								<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTen">Internat ou demi pension ? </a>
+							</h4>
+						</div>
+						<div id="collapseTen" class="panel-collapse collapse">
+							<div class="panel-body">
+								Les stages proposés en <strong>internat</strong> (pension complète) sont conçus de manière a ce que le stagiaire est un accompagnement maximal de l'équipe de formateur, des travaux de groupe sont également organisés en soirée ainsi que des animations. Le rythme du stage est adapté au groupe de stagiaire. Les stages en internat permettent de vivre une vraie vie en collectivité.
+
+								Les sessions en <strong>demie pension</strong> (restauration du midi) proposent une pédagogie active accès sur l'autonomie, en effet, des travaux de recherche, de préparation sont demandés a chaque stagiaire tous les soirs. 
+								
+							</div>
+						</div>
+					</div>	
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title"> <!-- Question date formation  -->
+								<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseEleven">Où trouver les dates de formation ?</a>
+							</h4>
+						</div>
+						<div id="collapseEleven" class="panel-collapse collapse">
+							<div class="panel-body">
+								Pour trouver le calendrier des <strong>stages BAFA</strong> cliquer <a class="color" href="http://localhost/projet/bafa.php" target="_blank"> ici </a>, calendrier des <strong>stages BAFD</strong> <a class="color" href="http://localhost/projet/bafd.php" target="_blank"> ici </a>
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title"> <!-- Question stage pratique-->
+								<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwelve">Où et comment trouver un stage pratique ?</a>
+							</h4>
+						</div>
+						<div id="collapseTwelve" class="panel-collapse collapse">
+							<div class="panel-body">
+								Ces questions sont traitées durant la période de formation et apporteront une aide concrète aux stagiaires. La FSCF travaille avec son réseau de partenaires locales et nationales afin de mettre en relation les organisateurs d'ACM et les stagiaires.
+								Cependant suivant la période, <strong>il est souvent souhaitable d'avoir déjà explorer quelques pistes avant même de rentrer en formation.</strong> Vous trouverez ici  la liste des organisateurs locaux.
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title"><!-- Question ressources pédagogiques -->
+								<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThirteen">Comment avoir accès aux ressources pédagogiques ?</a>
+							</h4>
+						</div>
+						<div id="collapseThirteen" class="panel-collapse collapse">
+							<div class="panel-body">
+								Les ressources pédagogiques sont accessibles à chaque stagiaire une fois sa <strong> session terminée</strong>. Vous y retrouverez les contenus théoriques de formation, les jeux, chants... ainsi que les photos de votre stage.   
+
+							</div>
+						</div>
+					</div>
+			</div>
+		</div>
+	</section>
+
+</div>		<!-- fermeture de la div container dans le header -->
+<?php require ('footer.php'); ?>
