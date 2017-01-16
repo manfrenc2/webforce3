32450c288af8ceed833dfc12b85dca774fa4dafd
Celine <celine-roy@orange.fr>
2017-01-16 12:27:07 +0100

    Responsive Dev Formateur 1.0.2

--- responsive/formateur.php
+++ responsive/formateur.php
@@ -1,51 +1,65 @@
-<!-- <h4 class="titleh4"></h4>-->
 <?php require ('header.php'); ?>			
-<div class="container">
-	<!-- debut section avec le texte -->
-	<section>
-<h1 class="titreFormateur">DEVENIR FORMATEUR BAFA/BAFD</h1>		
-		<div class="place">
-			<a href="#" class="btn" data-wipe="Formateur BAFA / BAFD">Formateur BAFA / BAFD </a> <br> 
-
-			<p>La FSCF défend des valeurs d'éducation populaire et recrute toute l'année partout en France des formateurs BAFA-BAFD souhaitant partager leurs connaissances, leurs expériences et leur passion pour l'animation.
-			<br/>
-			Le rôle du formateur BAFA-BAFD consiste à :
-			<br/>
-			&bull; Accompagner les stagiaires BAFA – BAFD à réfléchir sur leurs actions pédagogiques
-			<br/>
-			&bull; Développer les capacités de travail en équipe, d'écoute et d'attention aux autres
-			<br/>
-			&bull; Développer le respect d'autrui, l'esprit d’ouverture et de solidarité.
-			<br/>
-			&bull; Apporter ses connaissances et permettre a chacun des stagiaires de mettre en place des activités créatives et enrichissantes pour l'enfant.
-			<br/>
-			&bull; Transmettre des savoirs faire et des savoirs être sur l'organisation et l'animation de la vie quotidienne d'un Accueil Collectif de Mineurs.</p>
-			<br/>
-			<a href="#" class="btn" data-wipe="Comment devenir formateur ?">Comment devenir formateur ? </a> <br>
-			<br/>
-			<p>La première condition est d'être titulaire du BAFA et/ou du BAFD (ou équivalence), avoir une expérience significative et reconnue dans l’encadrement d'Accueil Collectif de Mineurs et être doté de véritables capacités d'expression orale.
-			<br/>
-			<br/>
-			Pour devenir formateur BAFA-BAFD à la FSCF, envoyer un CV accompagné d'une lettre de motivation. Un entretien de sélection est  ensuite effectué donnant accès à une formation de formateur initiale. </p>
-					
-	 		<br><br><a href="contact.php" class="myButton">Nous Contacter</a>
-	
-		</div>
-	</section>
-    <!-- fin de la section contenant du texte-->
-    <!-- debut section avec les dates et boutons  -->	
-	<section class="sectionmoove row">	
-		<div class="col-md-6 listplace">
-			<ul>
-				<h5><b>DATES:</b></h5>	
-				<li>item1</li>
-				<li>item2</li>
-				<li>item3</li>
-				<li>item4</li>
-			</ul>
-		</div>
-	</section>
-	<!-- fin section avec les dates et boutons -->
-</div>	
-</div>
+	<div class="container">
+		<!-- debut section avec le texte et photos -->
+		<section>		
+				<h1 class="titreFormateur">DEVENIR <BR>FORMATEUR<BR> BAFA/BAFD</h1>	
+				<div class="movequote"><!-- texte avec résumé valeurs de la FSCF -->
+					<br><p class="moovep"><i class="fa fa-quote-left left-quote" aria-hidden="true"></i> &nbsp;  La FSCF défend des valeurs d'éducation populaire et recrute toute l'année partout en France des formateurs BAFA-BAFD souhaitant partager leurs connaissances, leurs expériences et leur passion pour l'animation.</p><br>
+				</div>				
+				<div class="row"> 
+					<div class="hover11 col-md-6 col-xs-12 col-md-push-6"> <!-- première photo décallé sur la droite -->
+						 <figure><img src="img/devforma.jpg" class="imgforma img-fluid moovep" alt="photo devenir formateur"/></figure>	
+					</div> 
+					<div class="col-md-6 col-xs-12 col-md-pull-6 moovep" > <!-- div décalé sur la gauche -->
+						<div class="place"><a href="#" class="btn" data-wipe="Formateur BAFA / BAFD">Formateur BAFA / BAFD </a> <br> </div><br>
+						<br>
+						<p class="moovep">					
+							Le rôle du formateur BAFA-BAFD consiste à :
+							<br/>
+							&bull; Accompagner les stagiaires BAFA – BAFD à réfléchir sur leurs actions pédagogiques
+							<br/>
+							&bull; Développer les capacités de travail en équipe, d'écoute et d'attention aux autres
+							<br/>
+							&bull; Développer le respect d'autrui, l'esprit d’ouverture et de solidarité.
+							<br/>
+							&bull; Apporter ses connaissances et permettre a chacun des stagiaires de mettre en place des activités créatives et enrichissantes pour l'enfant.
+							<br/>
+							&bull; Transmettre des savoirs faire et des savoirs être sur l'organisation et l'animation de la vie quotidienne d'un Accueil Collectif de Mineurs.
+						</p>
+					</div>
+				</div>		
+				<div class="row">
+					<div class="hover11 column col-md-6 col-xs-12 moovep"> 
+						<figure><img src="img/DSC_0008.jpg" class="imgforma" alt="photo devenir formateur"/></figure> <!-- deuxième photo  -->
+					</div>
+					<div class="col-md-6 col-xs-12 moovep">
+						<div class="place"><a href="#" class="btn" data-wipe="Comment devenir formateur ?">Comment devenir formateur ?</a> <br> </div>
+						<br><br>
+						<p class="moovep">
+							La première condition est d'être titulaire du BAFA et/ou du BAFD (ou équivalence), avoir une expérience significative et reconnue dans l’encadrement d'Accueil Collectif de Mineurs et être doté de véritables capacités d'expression orale.
+							Pour devenir formateur BAFA-BAFD à la FSCF, envoyer un CV accompagné d'une lettre de motivation. Un 
+							entretien de sélection est  ensuite effectué donnant accès à une formation de formateur initiale. 
+						</p>
+						<br>
+						<a href="contact.php" class="myButton">Nous Contacter</a><!-- bouton contact  -->
+					</div>
+				</div>
+		</section>
+		<!-- fin de la section contenant du texte et des photos-->
+		
+		<!-- début section avec les dates -->	
+		<section class="sectionmoove row">	
+			<div class="col-md-12 listplace">
+				<ul>
+					<h5><b>DATES:</b></h5>	
+					<li>item1</li>
+					<li>item2</li>
+					<li>item3</li>
+					<li>item4</li>
+				</ul>
+			</div>
+		</section>
+	<!-- fin section avec les dates -->
+	</div>	
+</div> <!-- fermeture de la div container dans le header -->
 <?php require('footer.php')  ?>
