9b86282d0bc3e88ba837279db63cdcec1d288791
Celine <celine-roy@orange.fr>
2017-01-18 15:22:03 +0100

    BAFA responsive 1.0.1
    Nouvelle mise en forme de la page BAFA et travail sur le responsive

---
+++ responsive/bafa.php
@@ -0,0 +1,100 @@
+<?php require ('header.php'); ?>
+	<h1 class="titreBafa">BAFA</h1>					
+	<div class="container">
+		<section class="row">
+			<div class="col-md-10 col-xs-12 decalprogress"> 				 
+				<div>
+					<ul class="progressbar">
+					  <li class="active">la session de formation générale</li>
+					  <li>Le stage pratique</li>
+					  <li>la session d'approfondisement ou de qualification</li>					   
+					</ul>
+				 </div>
+			</div>
+		</section>
+		<section>
+			<div class="row"> 
+				<div class="hover11 col-md-6 col-xs-12 col-md-push-6"> <!-- première photo décallé sur la droite -->
+					 <figure><img src="img/DSC_0802.jpg" class="imgbafa img-fluid moovep" alt="photo bafa"/></figure>	
+				</div> 
+				<div class="col-md-6 col-xs-12 col-md-pull-6 moovep"> <!-- div texte décalé sur la gauche avec présentation du BAFA-->
+					<p class="moovep">					
+						<strong>Le BAFA</strong> est un brevet qui permet d'encadrer à titre non professionnel et de façon occasionnelle <strong>des enfants et adolescents </strong>en Accueils Collectifs de Mineurs (ACM).
+						Le BAFA est accessible à toute personne ayant <strong>17 ans révolus</strong> au premier jour de la session de formation générale et souhaitant intervenir dans le domaine de l'animation socioculturelle.
+						La formation BAFA prépare les futurs animateurs à :
+						<br/>
+						&bull; <strong>Assurer la sécurité</strong> physique et morale des mineurs.
+						<br/>
+						&bull; <strong>Participer au sein d'une équipe</strong> à la mise en œuvre d'un projet pédagogique en cohérence avec un projet éducatif dans le resoect du cadre réglementaire des ACM, se sensibiliser aux risques liés notamment aux conduites addictives et aux pratiques sexuelles.
+						<br/>
+						&bull; <strong>Construire une relation de qualité</strong> avec les mineurs, qu'elle soit individuelle ou collective
+						participer à l'accueil, à la communication et au développement des relations entre les différents acteurs, veiller notamment à prévenir toute forme de discrimination.
+						<br/>
+						&bull; <strong>Encadrer, animer </strong>la vie quotidienne et les activités.
+						<br/>
+						&bull; <strong>Accompagner les mineurs</strong> dans la réalisation de leurs projets.
+					</p>
+					<a href="inscription.php" class="myButton">S'inscrire au BAFA</a><!-- bouton contact  -->
+				</div>
+			</div>							
+		</section>
+		<hr/>
+		<section> <!-- section avec toutes les étapes  -->
+			<h4 class="titreBafa2">LES 3 Étapes:</h4>			
+			<div>		
+				<p><strong>La formation BAFA</strong>  se déroule en 3 parties et nécessite de suivre deux sessions de formations théoriques et un stage pratique qui se déroulent <strong>obligatoirement</strong> en France et dans l'ordre suivant :
+				</p>
+			</div>	
+			<div class="row">			
+				<div class="col-md-6 col-xs-12"><br> <!-- étape 1 -->				
+					<div class="place">						
+						<a class="btn btnresponsive1" data-wipe="#1 la session de formation">#1 la session de formation </a> <br> 
+						<br> 					
+						<p>D'une durée de <strong>8 jours minimum</strong>, la session de formation générale permet d'aborder des notions théoriques (connaissances de l'enfant et de l'adolescent, réglementation, notion de projet, etc) ainsi que des mises ne situation pratique (animation de jeune trvail en equipe, etc). La session de formation générale peut s'effectuer en internat ou en demi-pension. Si le directeur de stage vous délivre une <strong>appréciation favorable</strong>, vous pouvez alors prétendre a effecuter un stage pratique</p>		
+					</div>
+				</div>
+				<div class="col-md-6 col-xs-12"><br>	<!-- étape 2 -->			
+					<div class="place">
+						
+							<a class="btn" data-wipe="#2 Le stage pratique">#2 Le stage pratique </a> <br> 
+											
+						<br> <p>Le stage pratique s'effectue sur une durée de <strong>14 jours minimum</strong>. Il doit être réalisé dans un Accueil Collectif de Mineur (ACM) en France avec des enfants ou des adolescents. Il doit avoir lieu au plus tard 18 mois après le début de la session de formation générale. Si le directeur de la structure vous délivre une <strong>appréciation favorable</strong> vous pouvez alors vous inscrire à l'étape 3.</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6 col-xs-12"><br>	<!-- étape 3 -->		
+					<div class="place">
+						<a class="btn btnresponsive2" data-wipe="#3 la session d'approfondisement">#3 la session d'approfondisement</a> <br> 						
+						<br><p>La <b>session d'approfondissement</b> s'effectue  sur une durée d'au moins 6 jours. Elle permet de faire le bilan complet de la formation et de revenir sur l'expérience de stage pratique en échangeant avec les formateurs, les autres stagiaires. Cette session offre la possibilité d'approfondir des thématiques au choix tout en acquerant des techniques (jeu, chant, activités manuelles, etc), de connaitre les diffrents publics (petite enfance, adolescence, publics spécifiques, etc).
+						<br/></p>			
+					</div>
+				</div>
+				<div class="col-md-6 col-xs-12"><br>	<!-- étape 3 -->		
+					<div class="place">
+						
+							<a class="btn" data-wipe="#3 ou de qualification">#3 ou de qualification</a> <p><br> 
+							La <b>session de qualification </b> s'effectue sur une durée d'au moins 8 jours. Elle permet d'acquérir des compétences dans un domaine spécialisée. la FSCF propose la session de qualification "surveillance de baignade". 
+						A la fin de l'un de ces stages, si le directeur émet un avis favorable sur votre derniere session, alors votre dossier est examiné par le jury BAFA. Aprés deliberation, le candidat peut etre reçu, ajourné ou refusé. Le jury se réunit dans l'Yonne 2 fois par an.
+						</p>
+					</div>
+				</div>
+			</section>
+			<section> <!-- CALENDRIER DES DATES (à rajouter) -->	
+				<div class="boutondates">
+					<a href=""><button class="bouton6"> Découvrez notre calendrier de formation </button></a>
+				</div>
+				<div>
+					<ul>
+						<h5><b>Calendrier de dates</b></h5>	
+						<li>item1</li>
+						<li>item2</li>
+						<li>item3</li>
+						<li>item4</li>
+					</ul>
+				</div>
+			</section>
+		</div>
+	</div> 	
+</div><!-- fermeture de la div container du header  -->
+<?php require('footer.php')  ?>
