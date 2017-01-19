529ee726c41e7ab8c4b7898cf8539ecc146feb2f
Celine <celine-roy@orange.fr>
2017-01-19 14:24:07 +0100

    Responsive BAFD 1.0.1
    le fichier style est le même que pour BAFA

---
+++ responsive/bafd.php
@@ -0,0 +1,106 @@
+<?php require ('header.php'); ?>
+	<h1 class="titreGeneral barreplus">BAFD</h1>					
+	<div class="container">
+		<section class="row">
+			<div class="col-md-10 col-xs-12"> 				 
+				<div>
+					<ul class="progressbar">
+					  <li class="active">La session de formation générale</li>
+					  <li>Le 1er stage pratique</li>
+					  <li class="break">Session de perfectionnement</li>	
+					  <li>Le 2nd stage pratique</li>						  
+					</ul>
+				 </div>
+			</div>
+		</section>
+		<section>
+			<div class="row"> 
+				<div class="hover11 col-md-6 col-xs-12 col-md-push-6"> <!-- première photo décallé sur la droite -->
+					 <figure><img src="img/DSC_0802.jpg" class="imgbafa img-fluid moovep" alt="photo bafd"/></figure>	
+				</div> 
+				<div class="col-md-6 col-xs-12 col-md-pull-6 moovep"> <!-- div texte décalé sur la gauche avec présentation du BAFD-->
+					<p class="moovep">					
+							<br/>
+					Le <strong>Brevet d'Aptitude aux Fonctions de Directeur</strong> (BAFD) est un brevet de cadre intermédiaire non professionnel qui prépare à la mise en place d'outils de gestion administrative, logistique, financière, matérielle, sanitaire et humaine <strong>dans un accueil de loisirs ou un séjours de vacances</strong>. Ce brevet a pour finalité de développer une mission éducative et de diriger un Accueil Collectif de Mineurs (ACM).
+					<br/>
+					Il est accessible à toute  personne âgée <strong>de 21 ans révolus</strong> au premier jour de la session de formation et titulaire du BAFA ou d'un diplôme équivalent.
+					<br/>
+					Le BAFD et la spécificité des stages apportent un véritable « plus » d'expérience managériale. Il donne la possibilité aux futurs directeurs de <strong>diriger un ACM</strong> selon leurs éducatives et pédagogiques.
+					<br/>
+					La formation <strong>BAFD</strong> prépare les futurs directeurs à : 
+					<br/>
+					&bull; <strong>Situer son engagement</strong> dans le contexte social, culturel et éducatif.
+					<br/>
+					&bull; <strong>Conduire</strong> un projet pédagogique qui s'intègre dans un projet éducatif.
+					<br/>
+					&bull; <strong>Diriger</strong> les personnels.
+					<br/>
+					&bull; <strong>Assurer</strong> la gestion de l'accueil.
+					<br/>
+					&bull; <strong>Développer</strong> les partenariats et la communication.
+				</p>
+					<a href="inscription.php" class="myButton">S'inscrire au BAFD</a><!-- bouton contact  -->
+				</div>
+			</div>							
+		</section>
+		<hr/>
+		<section> <!-- section avec toutes les étapes  -->
+			<h4 class="titreGeneral2">LES 4 Étapes:</h4>			
+			<div>		
+				<p>L'obtention du BAFD nécessite de suivre deux sessions de formation théoriques et deux stages pratiques qui se déroulent obligatoirement en France et dans l'ordre suivant :
+ 				Ces 4 étapes de formation doivent être effectuées sur une durée maximale de 4 ans
+				</p>
+			</div>	
+			<div class="row">			
+				<div class="col-md-6 col-xs-12"><br> <!-- étape 1 -->				
+					<div class="place">						
+						<a class="btn btnresponsive1" data-wipe="#1 la session de formation">#1 la session de formation </a> <br> 
+						<br> 					
+						<p><strong>D'une durée de 9 jours consécutifs ou 10 jours</strong> en discontinus, la session de formation générale permet d'<strong>acquérir des notions théoriques</strong> (gestion d'un budget, notion d'économat, direction des personnels, etc). Cette session permet d'avoir des <strong>notions de bases</strong> aux fonctions de directeur et de construire le projet personnel de formation du candidat.</p>	
+					</div>
+				</div>
+				<div class="col-md-6 col-xs-12"><br>	<!-- étape 2 -->			
+					<div class="place">
+						
+							<a class="btn" data-wipe="#2 Le premier stage pratique">#2 Le premier stage pratique </a> <br> 
+											
+						<br> <p>Le stage pratique en tant que directeur ou adjoint de direction s'effectue sur une durée de<strong> 14 jours</strong>. Celui-ci doit être réalisé <strong>au maximum 18 mois après l'entrée en formation théorique</strong>. Ce stage est l'occasion de réaliser et de conduire son projet pédagogique au sein d'une équipe en <b>Accueil Collectif de Mineurs (ACM)</b>. Il permet de mettre en œuvre les acquis de la session de formation générale sur l'ensemble des fonctions exercées lors du stage.
+				</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6 col-xs-12"><br>	<!-- étape 3 -->		
+					<div class="place">
+						<a class="btn btnresponsive2" data-wipe="#3 La session de perfectionnement">#3 La session de perfectionnement</a> <br> 						
+						<br><p>La session de perfectionnement s'effectue sur une durée de <strong>6 jours</strong>. Elle permet de faire un premier bilan sur l'expérience acquise en stage pratique, <strong>de bénéficier d'un soutien dans l'écriture</strong> de son bilan de formation et de faire une mise à jour de ses savoirs. La FSCF propose des thématiques spécifiques.</p>
+						<br/>		
+					</div>
+				</div>
+				<div class="col-md-6 col-xs-12"><br>	<!-- étape 4 -->		
+					<div class="place">					
+							<a class="btn" data-wipe="#4 Le second stage pratique">#4 Le second stage pratique</a> 
+								<br><br><p>Le second stage pratique de direction s'effectue sur une durée de <span>14 jours</span> (uniquement en France) et doit <b>obligatoirement</b> être accompli en tant que directeur au sein d'un ACM. Il permet de perfectionner les aptitudes acquises au cours du cursus de formation. 
+				A l'issu de ce second stage, <strong>le candidat devra rédiger un bilan de formation</strong> sur la globalité du cursus. Celui est soumis à un jury de la Direction Régionale de la Jeunesse, des Sports et de la Cohésion (DRJSCS) en vue de l'obtention du Brevet d'Aptitude aux Fonctions de Directeur. 
+				</p>
+					</div>
+				</div>
+			</section>
+			<section> <!-- CALENDRIER DES DATES (à rajouter) -->	
+				<div class="boutondates">
+					<a><button class="bouton6"> Découvrez notre calendrier de formation </button></a>
+				</div>
+				<div>
+					<ul>
+						<h5><b>Calendrier de dates</b></h5>	
+						<li>item1</li>
+						<li>item2</li>
+						<li>item3</li>
+						<li>item4</li>
+					</ul>
+				</div>
+			</section>
+		</div>
+	</div> 	
+</div><!-- fermeture de la div container du header  -->
+<?php require('footer.php')  ?>
